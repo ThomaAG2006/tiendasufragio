8ee8ca9b45f652f4509e530e11d8a0492e7be692
f <f>
2022-05-18 17:28:23 -0500

    f2

--- controlador.js
+++ controlador.js
@@ -3,135 +3,415 @@ let producto1={
     nombre:"retrox-069",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/1.jpg?alt=media&token=81108bee-ef37-49dc-a3d0-17eefab5fbbd",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 
 let producto2={
     nombre:"visuz six",
-    precio:500000,
+    precio:550000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/10.jpg?alt=media&token=03d36f10-7b2e-4244-bc7b-c22413166cf5",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 
 let producto3={
     nombre:"nike1",
-    precio:500000,
+    precio:690000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/11.jfif?alt=media&token=741abeeb-9f19-42a3-9023-b7c8dd672275",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 let producto4={
     nombre:"Nike slurp",
-    precio:500000,
+    precio:580000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/12.jpg?alt=media&token=d8d1b39a-9426-4fb5-887a-8ba44bb84a8c",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 
 let producto5={
     nombre:"nike fitcral",
-    precio:500000,
+    precio:510000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/13.jpg?alt=media&token=54112ec1-f90c-4ad5-b899-15bc4fa5d905",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 
 let producto6={
     nombre:"nike t-11",
-    precio:500000,
+    precio:388000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/14.jpg?alt=media&token=dc6dedf3-3f3d-41a9-bbb3-1031a234a4b6",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 let producto7={
     nombre:"flipnik",
-    precio:500000,
+    precio:290000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/15.jpg?alt=media&token=256d5798-63e2-4fbc-8d88-928dcd0e2032",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 
 let producto8={
     nombre:"producto8",
-    precio:500000,
+    precio:240000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/16.jpg?alt=media&token=2c98f479-51ea-40c1-8ba3-25e5f9e324b8",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 
 let producto9={
     nombre:"producto9",
-    precio:500000,
+    precio:330000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/17.jpg?alt=media&token=bf15fa85-78b0-4ca7-bda8-c992ec17bd60",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 let producto10={
     nombre:"producto10",
-    precio:500000,
+    precio:190000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/18.jpg?alt=media&token=a7991b64-e9dc-40cd-9637-a893ea3e7cd4",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 
 let producto11={
     nombre:"producto11",
-    precio:500000,
+    precio:780000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/19.png?alt=media&token=b95346c2-52e6-48d3-b584-1d82d8a39618",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 
 let producto12={
     nombre:"producto12",
-    precio:500000,
+    precio:430000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/2.webp?alt=media&token=1a00cc38-2e99-4b76-a3ea-cd834d373ca8",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 let producto13={
     nombre:"producto13",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/21.jpg?alt=media&token=3f523d4f-7bf4-4494-a280-b0d801fca439",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 let producto14={
     nombre:"producto14",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/20.jpg?alt=media&token=070ce857-ef97-477a-9604-0e234f788c13",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 
 let producto15={
     nombre:"producto15",
-    precio:500000,
+    precio:900000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/21.jpg?alt=media&token=3f523d4f-7bf4-4494-a280-b0d801fca439",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 let producto16={
     nombre:"producto1",
-    precio:500000,
+    precio:180000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/23.jfif?alt=media&token=2a84b515-8304-4185-bb8f-bc9dbc5b0382",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 let producto17={
     nombre:"producto1",
-    precio:500000,
+    precio:470000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/24.jfif?alt=media&token=950aff6e-65fb-4ef8-9e53-c8a0a0a0dc06",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
 }
 let producto18={
     nombre:"producto1",
-    precio:500000,
+    precio:459000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/25.jfif?alt=media&token=8e23966d-fa94-4031-92c2-0c5e28af274d",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 let producto19={
     nombre:"producto1",
-    precio:500000,
+    precio:800000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/26.jfif?alt=media&token=583fd934-741d-4007-94b0-ba9d4df6f29a",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
 }
 let producto20={
     nombre:"producto1",
-    precio:500000,
+    precio:700000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/27.png?alt=media&token=eb50be13-5c7f-462d-baa2-7b3953cbce03",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto21={
+    nombre:"retrox-069",
+    precio:590000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/28.jpg?alt=media&token=aa5d0194-b9d0-4da0-a3b9-1b23808c651e",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto22={
+    nombre:"retrox-069",
+    precio:350000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/29.jfif?alt=media&token=54393b8f-1aea-4421-a1f2-346c85ca0b16",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto23={
+    nombre:"retrox-069",
+    precio:900000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/3.jpg?alt=media&token=83e33189-192a-4473-9726-9aaa329c527c",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto24={
+    nombre:"retrox-069",
+    precio:290000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/30.jfif?alt=media&token=d58a7728-3785-478e-8d6f-2b56c5b41a2c",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto25={
+    nombre:"retrox-069",
+    precio:280000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/31.jpg?alt=media&token=3fe558d5-2bfb-4e50-876c-331704cb4117",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto26={
+    nombre:"retrox-069",
+    precio:389000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/32.jfif?alt=media&token=239b57a3-f5ea-4911-a936-b5256b4c0367",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto27={
+    nombre:"retrox-069",
+    precio:100000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/33.jfif?alt=media&token=ca078c3e-5492-43cc-97f6-d7e87fbe3cde",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto28={
+    nombre:"retrox-069",
+    precio:485000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/34.jfif?alt=media&token=37de08d2-b6be-4f6c-b73c-4ffec0ccf595",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto29={
+    nombre:"retrox-069",
+    precio:517000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/35.jpg?alt=media&token=fa511557-6c4e-409d-abc7-2c16f17ef3fb",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto30={
+    nombre:"retrox-069",
+    precio:760000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/36.jpg?alt=media&token=bff24a71-5796-4e74-8181-3087a61f89fd",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto31={
+    nombre:"retrox-069",
+    precio:790000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/37.jpg?alt=media&token=51a2036b-6595-47e6-823f-edf179839089",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto32={
+    nombre:"retrox-069",
+    precio:852000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/38.jfif?alt=media&token=fdaed6aa-ea57-4f96-a527-0aab91f01a8c",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto33={
+    nombre:"retrox-069",
+    precio:984000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/39.jfif?alt=media&token=944f7ce2-4263-4a5e-81ae-f9232f7c69a9",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto34={
+    nombre:"flaws1",
+    precio:839000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/4.jfif?alt=media&token=66c3043f-8afe-42d0-9d51-5c78bd1f4ac1",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto35={
+    nombre:"flaws2",
+    precio:775000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/40.jpg?alt=media&token=88aeab4b-89ac-46e5-8aa5-340391037d04",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto36={
+    nombre:"flaws3",
+    precio:118000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/41.jfif?alt=media&token=fe0095d4-8dea-47fc-af41-6d220a47130e",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto37={
+    nombre:"troawll",
+    precio:105000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/42.jfif?alt=media&token=de7ed441-b79f-4171-9159-5374648501f4",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto38={
+    nombre:"xbennx",
+    precio:608000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/44.jpg?alt=media&token=d55464a2-e739-47f5-aca6-101a850c5cf7",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto39={
+    nombre:"aurstile",
+    precio:543000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/43.jfif?alt=media&token=9c392b1d-d88b-4074-aa66-2a773cfcd36f",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto40={
+    nombre:"london brex",
+    precio:785000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/45.jfif?alt=media&token=63ab4fc8-22ad-4ab1-af80-fbe3aeb51c43",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto41={
+    nombre:"buterfall",
+    precio:320000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/46.jpg?alt=media&token=4c01d7e3-153b-47a5-abaa-5ef893839e67",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto42={
+    nombre:"retrox-060",
+    precio:897000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/47.jfif?alt=media&token=e0000545-dc53-4758-bfe1-fc77ba0b2429",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto43={
+    nombre:"retrox-061",
+    precio:854000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/48.jpg?alt=media&token=e4ebd24d-0ee6-4b38-a5b3-722e9a6562de",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto44={
+    nombre:"retrox-062",
+    precio:239000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/49.jpg?alt=media&token=57a5dec9-6a5f-4939-a1a6-72b47b576ba6",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
+}
+let producto45={
+    nombre:"retrox-063",
+    precio:453000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/5.jfif?alt=media&token=396249fe-4189-4a44-a844-7fd9b1e75828",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto46={
+    nombre:"retrox-064",
+    precio:811000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/50.jfif?alt=media&token=05541afa-6e47-4757-b78a-3fa72f2e097d",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐"
+}
+let producto47={
+    nombre:"retrox-065",
+    precio:432000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/6.jfif?alt=media&token=fc2c2169-ce59-4fa1-9752-762e915f5fc1",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto48={
+    nombre:"retrox-066",
+    precio:908000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/7.jpg?alt=media&token=f288b936-e501-476c-a56b-845577b8153f",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto49={
+    nombre:"retrox-067",
+    precio:436000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/8.jpg?alt=media&token=21220230-e723-46a2-9251-79d24aeccae3",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐"
+}
+let producto50={
+    nombre:"retrox-068",
+    precio:200000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8d5df.appspot.com/o/9.jpg?alt=media&token=e5749b93-3938-4e0b-bbd1-f70af4dadb08",
+    pais:"mundialmente",
+    popularidad:"⭐⭐⭐⭐⭐"
 }
 
 //ARREGLO
@@ -155,11 +435,84 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
+
+
 )
 
 //RECORRO EL ARREGLO
+let contenedor=document.getElementById("contenedor")
 productos.forEach(function(producto){
     console.log(producto.nombre)
-})
+    
 
+    let foto=document.createElement("img")
+    foto.classList.add("w-100","img-fluid")
+    foto.src=producto.foto
+    
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    let nombreProducto=document.createElement("h5")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent=producto.nombre
+
+    let precioProducto=document.createElement("h5")
+    precioProducto.classList.add("text-center")
+    precioProducto.textContent=("$")+producto.precio
+
+    let estadoProducto=document.createElement("h5")
+    estadoProducto.classList.add("text-center")
+    estadoProducto.textContent=producto.estado
+    
+    let popularidadProducto=document.createElement("h6")
+    popularidadProducto.classList.add("ms-4")
+    popularidadProducto.textContent="Popularidad: "+producto.popularidad
+
+    let paisProducto=document.createElement("p")
+    paisProducto.classList.add("ms-4")
+    popularidadProducto.textContent="Pais: "+producto.pais
+    
+    tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(estadoProducto)
+    tarjeta.appendChild(popularidadProducto)
+    tarjeta.appendChild(popularidadProducto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+})
